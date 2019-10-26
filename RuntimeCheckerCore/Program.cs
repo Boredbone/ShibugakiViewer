890a39d9dda7b85be7db67a488191ec5810ad15f
boredbone <Boredbone@users.noreply.github.com>
2019-10-26 21:01:11 +0900

    RuntimeChecker

---
+++ RuntimeCheckerCore/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace RuntimeCheckerCore
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            var ver = Environment.Version;
+            var retVal = (ver.Major << 16) + (ver.Minor << 8) + ver.Build;
+            Console.WriteLine(retVal);
+            return retVal;
+        }
+    }
+}
---
+++ RuntimeCheckerDesctop/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace RuntimeCheckerDesctop
+{
+    class Program
+    {
+        [STAThread]
+        static int Main(string[] args)
+        {
+            var wpfLabel = new System.Windows.Controls.Label();
+            var formLabel = new System.Windows.Forms.Label();
+            var retVal = (formLabel.Height << 8) + (int)wpfLabel.FontSize;
+            Console.WriteLine(retVal);
+            return retVal;
+        }
+    }
+}
