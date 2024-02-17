3f3eb9864bd324baf413e763f3d5833c58fcc279
boredbone <boredbone.soft@gmail.com>
2024-02-17 18:30:55 +0900

    Folder selector dialog

--- ShibugakiViewer/Models/Utility/FolderSelector.cs
+++ ShibugakiViewer/Models/Utility/FolderSelector.cs
@@ -1,102 +1,71 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
 using System.Text;
 
 namespace ShibugakiViewer.Models.Utility
 {
-
     class FolderSelector
     {
         public List<string> SelectedItems { get; } = new List<string>();
-        public string LastSelectedPath { get; private set; }
+        public string? LastSelectedPath { get; private set; }
 
         public bool ShowDialog(string defaultPath)
         {
             this.LastSelectedPath = null;
             this.SelectedItems.Clear();
 
-            using (var fbd = new WindowsShell.FolderSelectDialog())
-            {
-                if (System.IO.Directory.Exists(defaultPath))
-                {
-                    fbd.DefaultDirectory = defaultPath;
-                }
+            var window = System.Windows.Application.Current.Windows
+                .OfType<System.Windows.Window>()
+                .SingleOrDefault(w => w.IsActive);
 
-                if (fbd.ShowDialog() == true)
-                {
-                    foreach (var item in fbd.SelectedItems)
-                    {
-                        if (!string.IsNullOrWhiteSpace(item))
-                        {
-                            this.SelectedItems.Add(item);
-                            this.LastSelectedPath = item;
-                        }
-                    }
-                }
+            var ofd = new Microsoft.Win32.OpenFolderDialog();
+            ofd.Multiselect = true;
+
+            if (!string.IsNullOrWhiteSpace(defaultPath)
+                && System.IO.Directory.Exists(defaultPath))
+            {
+                ofd.DefaultDirectory = defaultPath;
             }
 
-            return (this.SelectedItems.Count != 0);
-        }
-#if false
-        public bool ShowDialog2(string defaultPath)
-        {
-            this.LastSelectedPath = null;
-            this.SelectedItems.Clear();
-
-
-            string folderPath = null;
-            using (var fbd = new FolderSelectDialog2())
-            {
-                if (System.IO.Directory.Exists(defaultPath))
-                {
-                    fbd.DefaultDirectory = defaultPath;
-                }
-
-                if (fbd.ShowDialog() == true)
-                {
-                    folderPath = fbd.SelectedPath;
-                }
-            }
-
-            if (string.IsNullOrWhiteSpace(folderPath))
+            var result = (window == null) ? ofd.ShowDialog() : ofd.ShowDialog(window);
+            if (result != true)
             {
                 return false;
             }
 
-            try
+            foreach (var item in ofd.FolderNames)
             {
-                if ((".library-ms").Equals(Path.GetExtension(folderPath)))
+                string? folderPath = null;
+                if ((".library-ms").Equals(Path.GetExtension(item)))
                 {
-                    //Windowsライブラリの場合
-
-                    var libraryPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+                    var libraryPath = Path.Combine(
+                        Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
                         @"Microsoft\Windows\Libraries\");
                     var libraryName = Path.GetFileNameWithoutExtension
-                        (folderPath.Split(Path.DirectorySeparatorChar).Last());
+                        (item.Split(Path.DirectorySeparatorChar).Last());
 
-                    using (var shellLibrary
-                        = Microsoft.WindowsAPICodePack.Shell.ShellLibrary.Load(libraryName, libraryPath, true))
+                    using (var shellLibrary = WindowsShell.ShellLibrary.Load(libraryName, libraryPath, true))
                     {
                         foreach (var folder in shellLibrary)
                         {
-                            this.SelectedItems.Add(folder.Path);
-                            this.LastSelectedPath = folder.Path;
+                            folderPath = folder.Path;
                         }
                     }
                 }
                 else
                 {
-                    //通常フォルダ
-                    this.SelectedItems.Add(folderPath);
-                    this.LastSelectedPath = folderPath;
+                    folderPath = item;
+                }
+                if (!string.IsNullOrWhiteSpace(folderPath))
+                {
+                    this.SelectedItems.Add(item);
+                    this.LastSelectedPath = item;
                 }
             }
-            catch
-            {
-
-            }
-            return true;
+            return (this.SelectedItems.Count != 0);
         }
-#endif
     }
 }
